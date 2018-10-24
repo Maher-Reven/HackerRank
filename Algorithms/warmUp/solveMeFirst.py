59188bcd6059889af18d07f4d692ca35b44f7f72
Faktor BV <maher@faktor.io>
2018-10-24 16:50:48 +0200

    :boom: warmup a solveMeFirst  done

---
+++ Algorithms/warmUp/solveMeFirst.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+'''
+24 OCT, 2018
+@author: Graves88si(Maher Krde)
+'''
+def solveMeFirst(a,b):
+    # Hint: Type return a+b below
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
