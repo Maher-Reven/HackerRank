ea8ecd12e1b93c80731f90b23dd4c1e4e525ae6a
Faktor BV <maher@faktor.io>
2018-10-25 10:21:04 +0200

    :boom: d4 Classes  challenge  done

---
+++ 10DaysOfJs/d4Classes.js
@@ -0,0 +1,27 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+function Polygon(sides) {
+    this.sides = sides;
+    this.perimeter = function () {
+        var i;
+        var a = 0;
+        for (i = 0; i < (this.sides).length; i++) {
+
+            a = a + this.sides[i];
+        }
+        return a;
+    }
+}
+
+//2nd solution
+// class Polygon {
+//     constructor(sides) {
+//         this.sides = sides
+//     }
+//     perimeter() {
+//         return this.sides.reduce(function add(a, b) { return a + b; })
+//     }
+// }
