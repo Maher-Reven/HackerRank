28b85cc10cfc5ffba7ed94e4ba60ab3858756f82
Faktor BV <maher@faktor.io>
2018-10-26 09:50:37 +0200

    :boom: plus minus  challenge  done

---
+++ Algorithms/warmUp/plusMinus.py
@@ -0,0 +1,10 @@
+'''
+25 OCT, 2018
+@author: Graves88si(Maher Krde)
+'''
+
+n = int(input().strip())
+arr = [1 if int(temp)>0 else -1 if int(temp)<0 else 0 for temp in input().strip().split(' ') ]
+print("{0:.6f}".format(arr.count(1)/n))
+print("{0:.6f}".format(arr.count(-1)/n))
+print("{0:.6f}".format(arr.count(0)/n))
