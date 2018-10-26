5e8a4a8cb69581c78802266b597ad03c43534259
Faktor BV <maher@faktor.io>
2018-10-26 12:33:20 +0200

    :boom: d7 Regular Expressions II challenges done

---
+++ 10DaysOfJs/d7RegularExpressionsII.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function regexVar() {
+    var re = /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/
+
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+} 
+
