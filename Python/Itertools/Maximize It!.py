9ac67fe826d7cbf95393c58e3920ae5529daacd3
Faktor BV <maher@faktor.io>
2018-10-26 11:33:01 +0200

    :boom: maximaize it  challenge  done

---
+++ Python/Itertools/Maximize It!.py
@@ -0,0 +1,11 @@
+
+'''
+26 OCT, 2018
+@author: Graves88si(Maher Krde)
+'''
+from itertools import product
+
+K,M = map(int,input().split())
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
+print(max(results))
