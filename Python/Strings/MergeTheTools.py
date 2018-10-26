f659b479bf36f768b90f4f72800a02facd84dd27
Faktor BV <maher@faktor.io>
2018-10-26 17:36:18 +0200

    :boom:  Merge the Tools  challenges done

--- Python/Strings/MergeTheTools.py
+++ Python/Strings/MergeTheTools.py
@@ -11,3 +11,11 @@ for i in range(0, n, increment):
             finalstr = ''.join(templist)
         
     print(finalstr)
+
+
+
+#2nd solution
+S, N = input(), int(input()) 
+for part in zip(*[iter(S)] * N):
+    d = dict()
+    print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))    
