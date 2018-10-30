4dbdb5e1cca9d585710a3b29e4f12c349ad661eb
Faktor BV <maher@faktor.io>
2018-10-30 09:58:39 +0100

    :boom: Standardize Mobile Number Using Decorators  challenge done

---
+++ Python/ClosuresandDecorators/StandardizeMobileNumberUsingDecorators.py
@@ -0,0 +1,22 @@
+l = [input() for _ in range(int(input()))]
+
+def wrapper(f):
+    def phone(l):
+        f(["+91 "+c[-10:-5]+" "+c[-5:] for c in l])
+    return phone
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+sort_phone(l)
+
+
+
+#2nd solution
+
+def wrapper(f):
+    def fun(l):
+        decorated_l = ['+91 {} {}'.format(n[-10: -5], n[-5:]) for n in l]
+        return f(decorated_l)
+    return fun
