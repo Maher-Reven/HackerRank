5f61cdeea6049cfbe7026af9f5e2eadf57eb8a07
Faktor BV <maher@faktor.io>
2018-11-05 09:48:54 +0100

    :boom: sum and prod challenge done

---
+++ Python/Numpy/SumAndProd.py
@@ -0,0 +1,12 @@
+import numpy
+f = []
+input()
+while True:
+    try:
+        o = [ int(x) for x in input().split(' ')]
+        f.append(o)
+    except:
+        break
+f = numpy.array(f)
+s = f.sum(axis = 0)
+print(s.prod(axis = 0))
