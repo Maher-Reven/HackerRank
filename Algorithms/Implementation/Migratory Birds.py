dbd694b4656de22660a7c23c14fca9ca85bf6208
Faktor BV <maher@faktor.io>
2018-12-06 15:48:47 +0100

    :boom: Day of the Programmer challenge done

--- Algorithms/Implementation/Migratory Birds.py
+++ Algorithms/Implementation/Migratory Birds.py
@@ -11,3 +11,17 @@ while i<len(countList):
         result.append(ar[i])
     i+=1
 print(min(result))
+
+
+
+#2nd solution
+
+raw_input()
+types = map(int, raw_input().split())
+
+bird_count = {}
+
+for typ in types:
+    bird_count[typ] = bird_count.get(typ, 0) + 1
+    
+print max(bird_count, key=lambda x: bird_count[x])
