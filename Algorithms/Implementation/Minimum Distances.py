d5532f053a1f7299d568db0d75000574d0dcd9f0
Faktor BV <maher@faktor.io>
2018-12-07 16:08:44 +0100

    :boom: Minimum Distances challenge done

---
+++ Algorithms/Implementation/Minimum Distances.py
@@ -0,0 +1,8 @@
+def minimum_distance(n, A):
+    for d in range(1, n):
+        for i in range(n - d):
+            if A[i] == A[i+d]:
+                return d
+    return -1
+
+print(minimum_distance(int(input()), [int(x) for x in input().split()]))
