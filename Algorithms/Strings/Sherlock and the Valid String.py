70dd03a5e377f968a932e9425657de46ed33873c
Faktor BV <maher@faktor.io>
2018-12-10 12:08:43 +0100

    more optimaized code in py3

--- Algorithms/Strings/Sherlock and the Valid String.py
+++ Algorithms/Strings/Sherlock and the Valid String.py
@@ -1,7 +1,45 @@
-S=map(str,raw_input())
-s=set(S)
+#!/bin/python3
 
-if((len(S)%len(s)==0) or ((len(S)-1)%(len(s)-1)==0) or (len(S)%len(s)==1)):
-    print "YES"
-else:
-    print "NO"
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the isValid function below.
+def isValid(s):
+    # Time : O(N log N)
+    # Space : O(N)
+    # Method : sort the frequency values of characters and try decrementing the last element
+    d = {}
+    for l in s:
+        if l in d.keys():
+            d[l] += 1
+        else:
+            d[l] = 1
+    
+    f = sorted(d.values())
+    decision = "NO"
+    print(f)
+    if len(set(f)) == 1:
+        decision = "YES"
+    else:
+        if (f[0]- 1 == 0):
+            f.pop(0)
+        elif (f[-1] - 1 == f[-2]):
+            f[-1] -= 1
+        if len(set(f)) == 1:
+            decision = "YES"
+            
+    return decision
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = isValid(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
